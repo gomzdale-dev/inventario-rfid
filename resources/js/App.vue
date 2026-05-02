71792c1b7edf8fe911a451c45679d5c505a33944
gomzdale-dev <gomzdale26@gmail.com>
2026-05-02 03:39:10 -0600

    feat: login institucional con logo y animaciones- pantalla lista para trabajar backend

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,14 +1,31 @@
 <template>
-  <Dashboard />
+  <Login
+    v-if="!isAuthenticated"
+    @login-success="login"
+  />
+
+  <Dashboard v-else />
 </template>
 
 <script>
+import Login from "./Pages/Login.vue"
 import Dashboard from "./Pages/Dashboard.vue"
 
 export default {
   name: "App",
   components: {
+    Login,
     Dashboard
+  },
+  data() {
+    return {
+      isAuthenticated: false
+    }
+  },
+  methods: {
+    login() {
+      this.isAuthenticated = true
+    }
   }
 }
 </script>
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,6 +4,19 @@
     <meta charset="UTF-8">
     <title>Inventario RFID</title>
     @vite('resources/js/app.js')
+
+    <style>
+        * {
+            box-sizing: border-box;
+        }
+
+        html,
+        body {
+            margin: 0;
+            min-height: 100%;
+            font-family: Arial, Helvetica, sans-serif;
+        }
+    </style>
 </head>
 <body>
     <div id="app"></div>
